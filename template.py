87ad01de3cef00ae598029c6e9b16877063b8b76
Swati81 <sinhaswati025@gmail.com>
2021-03-18 14:03:34 +0530

    params added

--- template.py
+++ template.py
@@ -1,15 +1,19 @@
 import os
+
+
 dirs = [
-    os.path.join("data","raw"),
+    os.path.join("data", "raw"),
     os.path.join("data","processed"),
     "notebooks",
     "saved_models",
     "src"
 ]
-for dir in dirs:
-    os.makedirs(dir,exist_ok=True)
-    with open(os.path.join(dir,".gitkeep"),"w") as f:
+
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
         pass
+
 
 files = [
     "dvc.yaml",
@@ -17,6 +21,7 @@ files = [
     ".gitignore",
     os.path.join("src","__init__.py")
 ]
-for file in files:
-    with open(file,"w") as f:
+
+for file_ in files:
+    with open(file_, "w") as f:
         pass
